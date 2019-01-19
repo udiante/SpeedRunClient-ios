99fb814c1c229d8ecec7b05584bde228a312de4d
Alejandro Quibus <udiante@gmail.com>
2019-01-19 19:07:16 +0100

    Initial Commit

---
+++ SpeedRunClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedRunClient
+//
+//  Created by Alejandro Quibus on 19/01/2019.
+//  Copyright © 2019 Alejandro Quibus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

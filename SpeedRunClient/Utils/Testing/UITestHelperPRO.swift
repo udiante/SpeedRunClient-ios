f8f99a9058c6f513b0df1a86b3f2c0d2abe18cc5
Alejandro Quibus <udiante@gmail.com>
2019-01-20 23:42:10 +0100

    Added PRO Target

---
+++ SpeedRunClient/Utils/Testing/UITestHelperPRO.swift
@@ -0,0 +1,15 @@
+//
+//  UITestHelperPRO.swift
+//  SpeedRunClientPRO
+//
+//  Created by Alejandro Quibus on 20/01/2019.
+//  Copyright © 2019 Alejandro Quibus. All rights reserved.
+//
+
+import Foundation
+
+class UITestHelper {
+    static func configure(){
+        
+    }
+}
